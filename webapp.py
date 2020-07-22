ce5feae46664bc8e395891c826933648f58109ab
arth <arth.dharaskar97@gmail.com>
2020-07-22 13:05:23 -0700

    Updated webapp

--- webapp.py
+++ webapp.py
@@ -15,16 +15,16 @@ def respond():
 
     response = {}
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
+    # # Check if user sent a name at all
+    # if not name:
+    #     response["ERROR"] = "no name found, please send a name."
+    # # Check if the user entered a number not a name
+    # elif str(name).isdigit():
+    #     response["ERROR"] = "name can't be numeric."
+    # # Now the user entered a valid name
+    # else:
+    #     response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+    response["MESSAGE"] = jsonify(json_dump)
     # Return the response in json format
     return jsonify(json_dump)
 
